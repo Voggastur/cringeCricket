2a54d7408fdfb7ecee42f6d729b3a0ca8c49720f
Voggastur <v3x0r_x014@hotmail.com>
2020-07-12 01:07:22 +0000

    new tavern img, slider functional

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(".modal").modal();
-  $(".card").css("margin", "0, 20px").css("color", "white");
   $(".carousel").carousel();
+  $(".card").css("margin", "0, 20px").css("color", "white");
   const slider = document.querySelector(".slider");
   M.Slider.init(slider, {
     indicators: false,
-    height: 500,
+    height: 600,
     transition: 500,
     interval: 6000,
   });
