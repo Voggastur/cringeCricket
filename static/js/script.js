952e0fb953f27dd8469d9bcbad29c6986424f1c0
Voggastur <v3x0r_x014@hotmail.com>
2020-07-18 21:02:14 +0000

    all

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
   $(".modal").modal();
   $("select#adventure").formSelect();
   $("#possessions").characterCounter();
-
+  $(".materialboxed").materialbox(function {
+    $(".card").onOpenStart(function() {
+        $(this).find("p").css("transition", "all 0.5s ease").css("display", "block").css("visibility", "visible");
+        });
+    });
+  
   // My functions
   const slider = document.querySelector(".slider");
   M.Slider.init(slider, {
@@ -12,15 +17,9 @@ $(document).ready(function () {
     transition: 500,
     interval: 6000,
   });
-  $(".card").click(function () {
-    $(".materialboxed").materialbox();
-  });
-  //  $(".materialboxed").onOpenStart()
+  
   //  $(".materialboxed").onCloseStart()
-
-  //
   //    $(this).wrap( "<div class='hero_wrap'></div>" );
   //    $(".hero_wrap").css("transition", "all 0.5s ease").css("position", "absolute").css("width", "100vw").css("height", "100vh").css("background-color", "#000");
-  //    $(this).css("transition", "all 0.5 ease").css("position", "absolute").css("width", "75vw").css("height", "75vh").css("z-index", "2");
-  //    $(this).find(".card-items p").css("transition", "all 0.5s ease").css("display", "block").css("visibility", "visible");
+
 });
