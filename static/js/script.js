a6e5b5b75cfa2d5e0e65b771cef53dd17e7ce797
Voggastur <v3x0r_x014@hotmail.com>
2020-07-19 13:09:45 +0000

    addition

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,35 @@ $(document).ready(function () {
   $(".modal").modal();
   $("select#adventure").formSelect();
   $("#possessions").characterCounter();
-  $('.materialboxed').materialbox({
-    inDuration:200,
-    outDuration:200,
-    onOpenEnd: function() {$(this).children("p", "span").removeClass(".visibility")},
-    onCloseEnd: function() {$(this).children("p", "span").addClass(".visibility")}
-});
-  
+  // Materialize materialbox() with my options
+  $(".materialboxed").materialbox({
+    inDuration: 200,
+    outDuration: 200,
+    onOpenEnd: function () {
+      $(".materialboxed").find("p").removeClass(".visibility");
+    },
+    onCloseStart: function () {
+      $(".materialboxed").find("p").addClass(".visibility");
+    },
+  });
+  // This function limits input in possessions which tend to be longer
+  const textLimitPossess = 80;
+  $("textarea#possessions").keyup(function () {
+    var tlength = $(this).val().length;
+    $(this).val($(this).val().substring(0, textLimitPossess));
+    var tlength = $(this).val().length;
+    remain = textLimitPossess - parseInt(tlength);
+    $("#remain").text(remain);
+  });
+  // This function limits input in adventure_topic which tend to be longer
+  const textLimitAdventure = 200;
+  $("textarea#adventure_topic").keyup(function () {
+    var tlength = $(this).val().length;
+    $(this).val($(this).val().substring(0, textLimitAdventure));
+    var tlength = $(this).val().length;
+    remain = textLimitAdventure - parseInt(tlength);
+    $("#remain").text(remain);
+  });
   // My functions
   const slider = document.querySelector(".slider");
   M.Slider.init(slider, {
@@ -18,7 +40,8 @@ $(document).ready(function () {
     transition: 500,
     interval: 6000,
   });
-  
+
+  // $("#div_id").prop("maxlength", "80");
   //  $(".materialboxed").onCloseStart()
   //    $(this).wrap( "<div class='hero_wrap'></div>" );
   //    $(".hero_wrap").css("transition", "all 0.5s ease").css("position", "absolute").css("width", "100vw").css("height", "100vh").css("background-color", "#000");
