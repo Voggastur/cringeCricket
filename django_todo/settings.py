73b5057258f3f07e5b4d0abad5c74ff78864f094
Voggastur <v3x0r_x014@hotmail.com>
2020-08-03 18:24:22 +0000

    add

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "htc^(xu7_1*mmi7&8)knt4@pa@u7jvv%u6por=5j(49qp**^nt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
