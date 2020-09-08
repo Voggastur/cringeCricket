fef7ca1cfd166aecf138538f7bca19315b2f0635
Voggastur <v3x0r_x014@hotmail.com>
2020-09-08 23:11:10 +0000

    django practice

--- mywebproject/urls.py
+++ mywebproject/urls.py
@@ -1,7 +1,7 @@
-"""django_todo URL Configuration
+"""mywebproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from todo import views
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', views.get_todo_list, name='get_todo_list'),
-    path('add', views.add_item, name='add'),
-    path('edit/<item_id>', views.edit_item, name='edit'),
-    path('toggle/<item_id>', views.toggle_item, name='toggle'),
-    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
